b40d9381b0625f14137e9faa9243939d631fc986
2smarthrm <2smarthrm@gmail.com>
2025-03-26 09:05:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.post("/sendfile", async (req, res) => {
     const mailOptions = {
       from: "noreply@marketing.exportech.com.pt",
       to: [Data.email],
-       bcc:["kiosso.silva@exportech.com.pt"],
+       bcc:["geral@exportech.com.pt"],
       subject: `Formulário de Devolução - ${Data.company || "Não informado"}`,
       text: `Segue em anexo o formulário de devolução da empresa ${Data.company || "Não informado"}.`,
       attachments: [{ filename: "Formulario_Devolucao.pdf", content: pdfBytes, contentType: "application/pdf" }],
