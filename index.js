288bc4408edc126e45c51c405d78ffba7f8cbef5
2smarthrm <2smarthrm@gmail.com>
2025-06-12 13:07:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ function Managers(managerKey) {
     return emailRegex.test(email);
   }
 
-  if (isValidEmail(mail) === false) return "kiossocamuegi@gmail.com"  /* fabio.catela@exportech.com.pt */;
+  if (isValidEmail(mail) === false) return "kiossocamuegi@gmail.com" ;
   console.log("key = ", newKey);
-  return mail !== null ? return "kiossocamuegi@gmail.com"  : mail; /* fabio.catela@exportech.com.pt */
+  return mail !== null ? return "kiossocamuegi@gmail.com"  : mail;  
 }
 
 const app = express();
